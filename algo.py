618c64fa1daac0650eed0be721893f9b7035e57b
noguiixz7 <noguilhermek@gmail.com>
2026-04-08 11:35:57 -0300

    codigo final

--- algo.py
+++ algo.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+# Programa: Lista com dobro e triplo
+
+original = []
+modificada = []
+
+print("Digite 30 números inteiros:")
+for i in range(30):
+    numero = int(input(f"numero {i+1}: "))
+    original.append(numero)
+
+for i in range(30):
+    valor_atual = original[i]
+    
+    if i % 2 == 0:
+        novo_valor = valor_atual * 2
+    else:
+        novo_valor = valor_atual * 3
+        
+    modificada.append(novo_valor)
+
+print("Lista Original:", original)
+print("Lista Modificada:", modificada)
