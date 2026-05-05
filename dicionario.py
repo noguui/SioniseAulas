bf89d86d217cd799b7b922f9cd1ac9c23c28e919
noguiixz7 <noguilhermek@gmail.com>
2026-05-05 11:28:13 -0300

    dicionario mais organizado

--- dicionario.py
+++ dicionario.py
@@ -8,3 +8,7 @@ print(pessoa)
 for chave, valor in pessoa.items():
     print(f"{chave} é {valor}")
 
+for chave in pessoa.keys():
+    print(f'A chave é {chave}')
+for valor in pessoa.values():
+    print(f'O valor da chave é {valor}')
