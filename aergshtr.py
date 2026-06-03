3fb5fc0a67a95c11e68f4a2116ba5f650e84f03c
guigazx7 <noguilhermek@gmail.com>
2026-06-02 21:23:19 -0300

    verdade

---
+++ aergshtr.py
@@ -0,0 +1,21 @@
+#resposta da atividade da venda de didas
+dida = [
+    {
+        "sabor":"ninho com nutella",
+        "preco": 4,
+        "quantidade": 10
+    },
+    {
+        "sabor":"morango com nutella",
+        "preco": 2,
+        "quantidade": 8
+    },
+    {
+        "sabor":"prestígio",
+        "preco": 3,
+        "quantidade": 5
+    }
+]
+for i in dida:
+    print("sabor:", i ["sabor"])
+    print("preço:", i ["preco"])
