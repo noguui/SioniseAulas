118f5f4fc7833d8a3b4b907037cacfd1b5e74fc3
Noguixz7 <noguilhermek@gmail.com>
2026-08-27 11:57:48 -0300

    terminado1

--- POO/POOFlix.py
+++ POO/POOFlix.py
@@ -11,16 +11,6 @@ class Filme:
         print("\n"*3)
         print(f"----Filme---- \nAno: {self.ano}, \nDiretor: {self.diretor}, \nClassificação: {self.classificaçao},\nAtores: {self.atores}, \nSinopse: {self.sinopse}, \nTitulo: {self.titulo}, \nGenero: {self.genero}")
         print("\n"*3)
-Filme1 = Filme(
-    "2008",
-    "joao elisue",
-    "9+",
-    "juliao do grau e julinho do peao",
-    "cawboys perdidos no espaço por causa de algo sobrenatural estranho",
-    "Cawboys perdidos",
-    "ação"
-    )
-Filme1.exibir_informacoes()
 
 class Serie:
     def __init__(self, ano, diretor, classificacao, atores, sinopse, titulo, genero ):
@@ -35,16 +25,7 @@ class Serie:
         print("\n"*3)
         print(f"----Serie---- \nAno: {self.ano}, \nDiretor: {self.diretor}, \nClassificação: {self.classificaçao}, \nAtores: {self.atores}, \nSinopse: {self.sinopse}, \nTitulo: {self.titulo}, \nGenero: {self.genero}")
         print("\n"*3)
-Serie1 = Serie(
-    "2011",
-    "Lola La Cava",
-    "18+",
-    "Castiel, Nathaniel, Lysandre, Kentin e Armin.",
-    "Um estranho amor na escola",
-    "Amor Doce",
-    "Romance, Drama"
-    )
-Serie1.exibir_informacoes()
+
 
 class Usuario:
     def __init__(self, email, idade, cartao_bancario):
@@ -55,9 +36,34 @@ class Usuario:
         print("\n"*3)
         print(f"----Usuario---- \nEmail: {self.email}, \nIdade: {self.idade}, \nDados bancarios: {self.cartao_bancario} ")
         print("\n"*3)
-Usuario1 = Usuario(
+
+
+#Instacia - Objetos
+filme1 = Filme(
+    "2008",
+    "joao elisue",
+    "9+",
+    "juliao do grau e julinho do peao",
+    "cawboys perdidos no espaço por causa de algo sobrenatural estranho",
+    "Cawboys perdidos",
+    "ação"
+    )
+filme1.exibir_informacoes()
+
+serie1 = Serie(
+    "2011",
+    "Lola La Cava",
+    "18+",
+    "Castiel, Nathaniel, Lysandre, Kentin e Armin.",
+    "Um estranho amor na escola",
+    "Amor Doce",
+    "Romance, Drama"
+    )
+serie1.exibir_informacoes()
+
+usuario1 = Usuario(
     "glauberdoido@gmail.com",
     "67",
     "12345678765, 54456, 07/54, 434"
     )
-Usuario1.exibir_informacoes()
+usuario1.exibir_informacoes()
